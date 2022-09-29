4e7c57f524ccac574003963a31eb8ac183d1b55a
MMK-13 <kantemamadou13@gmail.com>
2022-09-29 04:06:42 +0000

    Finalizing

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
-	<h1>Добро пожаловать!</h1>
-	<button @click="showPopUp();">Click me!</button>
-	<PopUp :reveal="revealModal" @toggle="toggle" />
+	<div class="app">
+		<h1 class="app__title">Welcome!</h1>
+		<button @click="showPopUp();" class="app__btn">Click me!</button><br><br>
+		<PopUp :reveal="revealModal" @toggle="toggle" />
+	</div>
 </template>
 
 <script>
 	import PopUp from './components/PopUp.vue'
-
+	
 	export default {
 		name: 'App',
 		data: () => ({
-			revealModal: true,
+			revealModal: false
 		}),
 		components: {
 			PopUp
 		},
 		methods: {
 			showPopUp() {
-				this.revealModal = true;
+				this.revealModal = !this.revealModal;
 			},
 			toggle(payload) {
 				this.revealModal = payload.reveal;
@@ -26,13 +28,7 @@
 	}
 </script>
 
-<style>
-	#app {
-		font-family: Avenir, Helvetica, Arial, sans-serif;
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-		text-align: center;
-		color: #2c3e50;
-		margin-top: 60px;
-	}
+<style scoped>
+	@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap');
+	@import url('./components/css/App.css');
 </style>
--- src/components/PopUp.vue
+++ src/components/PopUp.vue
@@ -14,14 +14,25 @@
 					Subscrive to recieve 10% off promocode plus exclusive offers and deals
 				</div>
 				<div class="card__subscribing">
-					<label for="email" class="card__subscribing-email">Email-adress</label>
-					<input id="email" type="email" class="card__subscribing-input">
-					<button class="card__subscribing-button">Subscribe!</button>
+					<label for="email" class="card__subscribing-email">
+						Email-adress
+						<span :class="classes">{{ emailExist.text }}</span>
+					</label>
+					<input id="email" type="email" class="card__subscribing-input" v-model="email">
+					<button class="card__subscribing-button" @click="subscribe()">Subscribe!</button>
 				</div>
 				<div class="card__privacy">
 					<input id="privacy" type="checkbox" class="card__privacy-input">
 					<label for="privacy" class="card__privacy-label">I'm agree with privacy policy</label>
 				</div>
+			</div>
+			<div class="card__right">
+				<div class="card__images">
+					<img src="@/assets/bg-img.png" class="card__image" />
+					<img src="@/assets/rectangle.png" class="card__backimage back-image-1" />
+					<img src="@/assets/rectangle.png" class="card__backimage back-image-2" />
+				</div>
+				<button class="card__close" @click="hide();">X</button>
 			</div>
 		</div>
 	</div>
@@ -31,152 +42,45 @@
 <script type="text/javascript">
 	export default {
 		name: 'PopUp',
-		props: [
-		'reveal'
-		],
+		data: () => ({
+			email: '',
+			emails: [],
+			emailExist: {
+				text: '',
+				type: ''
+			}
+		}),
+		props: ['reveal'],
 		methods: {
 			hide() {
 				this.$emit('toggle', {
-					reveal: !this.$props.reveal
-				})
+					reveal: !this.$props.reveal,
+				});
+			},
+			subscribe() {
+				let e = this.emails.find((em) => em === this.email)
+				if (e === undefined) {
+					this.emails.push(this.email)
+					this.email = ''
+					this.emailExist.type = 'success'
+					this.emailExist.text = 'You have successfully subscribed to the newsletter'
+				} else {
+					this.emailExist.type = 'danger'
+					this.emailExist.text = 'You have already subscribed to the newsletter'
+				}
+				console.log(e)
+			}
+		},
+		computed: {
+			classes() {
+				return this.emailExist.type
 			}
 		}
 	}
 </script>
 
 
-<style type="text/css">
-	@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap');
+<style type="text/css" scoped>
 	@import url('./css/PopUp.css');
-	.popup {
-		text-align: left;
-		font-family: 'Roboto', sans-serif;
-		position: fixed;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		top: 0;
-		border: 0;
-		left: 0;
-		right: 0;
-	}
-	.overlay {
-		position: fixed;
-		top: 0;
-		bottom: 0;
-		left: 0;
-		right: 0;
-		background-color: rgba(0, 0, 0, 0.75);
-	}
-	.card {
-		width: 864px;
-		background-color: #FFF;
-		color: #333;
-		padding: 40px 32px;
-		position: fixed;
-		top: 50px;
-		border-radius: 8px;
-		box-shadow: 0px 8px 20px 0px #444B4D26;
-	}.card__btn {
-		position: absolute;
-		top: 10px;
-		right: 10px;
-		display: inline-block;
-		border: 1px solid #red;
-		border-radius: 4px;
-		background-color: rgb(255, 25, 25);
-		color: #fff;
-		padding: 6px 8px;
-		font-weight: 400;
-		font-size: 16px;
-		cursor: pointer;
-	}
-	/* Beginning */
-	.card__title {
-		font-weight: 700;
-		font-size: 104px;
-		line-height: 104px;
-	}
-	.card__title span {
-		font-size: 24px;
-		color: #2F3639;;
-	}
-	.card__subtitle {
-		font-weight: 700;
-		font-size: 24px;
-		line-height: 100%;
-		margin-top: 16px;
-		color: #828688;
-	}
-	.card__left {
-		width: 416px;
-	}
-	.separator {
-		width: 67px;
-		height: 2px;
-		background: #C24DFE;
-		border-radius: 10px;
-		margin: 26px 0;
-	}
-	.card__text {
-		font-weight: 400;
-		font-size: 14px;
-		line-height: 120%;
-		color: #595E61;
-		width: 373px;
-		margin-bottom: 40px;
-	}
-	.card__subscribing {
-		display: flex;
-		flex-direction: column;
-	}
-	.card__subscribing-email {
-		font-weight: 700;
-		font-size: 12px;
-		line-height: 14px;
-		color: #828688;
-		margin-bottom: 8px;
-	}
-	.card__subscribing-input {
-		height: 20px;
-		width: 302px;
-		border: 1px solid #D5D7D7;
-		border-radius: 6px;
-		padding: 10px 16px;
-		margin-bottom: 16px;
-	}
-	.card__subscribing-input:focus, .card__subscribing-input:hover {
-		outline: #C24DFE;
-		border: 1px solid #C24DFE;;
-	}
-	.card__subscribing-button {
-		width: 106px;
-		height: 40px;
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 13px 24px;
-		gap: 10px;
-		background: #C24DFE;
-		border-radius: 35px;
-		font-weight: 700;
-		font-size: 12px;
-		line-height: 14px;
-		color: #FFF;
-		border: none;
-		margin-bottom: 16px;
-	}
-	.card__privacy {
-		display: flex;
-		flex-direction: row;
-		align-items: center;
-	}
-	.card__privacy-label {
-		font-weight: 500;
-		font-size: 14px;
-		line-height: 16px;
-		color: #595E61;
-		margin-left: 12px;
-	}
+	@import url('./css/PopMessage.css');
 </style>
